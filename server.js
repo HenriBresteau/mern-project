31213166d48cb898172d1049149ffce6d9a49824
henriB <henri.bretseau@gmail.com>
2020-11-26 18:24:16 +0100

    auth middelware

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path:'./config/.env'});
 require('./config/db');
+const checkUser = require('./middleware/auth.middleware');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
+
+// JWT
+app.get('*', checkUser);
 
 // ROUTES
 app.use('/api/user', userRoutes);
