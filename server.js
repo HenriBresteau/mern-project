dd5640d4ce5689b2984d4563c1e5fbc5763fad98
henriB <henri.bretseau@gmail.com>
2020-11-26 11:43:03 +0100

    user Routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path:'./config/.env'});
 require('./config/db');
 const app = express();
