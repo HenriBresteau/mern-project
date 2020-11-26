c2ece69956b4628696b3dbd800734a31f6ce5ee2
henriB <henri.bretseau@gmail.com>
2020-11-26 17:58:50 +0100

    signIn + jwt + createToken

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,12 @@
 const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+const maxAge = 3*24*60*1000
+const createToken= (id)=>{
+    return jwt.sign({id}, process.env.TOKEN_SECRET,{
+        expiresIn: maxAge
+    })
+}
 
 module.exports.signUp = async (req, res)=>{
     console.log(req.body);
@@ -13,6 +21,19 @@ module.exports.signUp = async (req, res)=>{
         res.status(200).send({error})
     }
 }
+module.exports.signIn = async (req, res) =>{
+    const {email,password} = req.body
+
+    try {
+        const user = await UserModel.login(email,password);
+        const token = createToken(user._id);
+        res.cookie('jwt',token, {httpOnly: true, maxAge:maxAge})
+        res.status(200).json({user:user._id})
+    } catch (error) {
+        res.status(200).json(error);
+    }
+}
+
 module.exports.logout = async (req, res )=> {
 
 }
