f58ee537928b2778eb924e5486429d02e64ced93
henriB <henri.bretseau@gmail.com>
2020-11-26 12:26:58 +0100

    on delete by :id -> deleteUser

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,6 +10,7 @@ router.post('/register', authController.signUp);
 router.get('/', userController.getAllUsers)
 router.get('/:id', userController.userInfo)
 router.put('/:id', userController.userUpdate)
+router.delete('/:id', userController.deleteUser)
 
 
 module.exports = router;
