fa1742c3196b86c6d1984e6c165161d0770e3688
henriB <henri.bretseau@gmail.com>
2020-11-26 11:18:35 +0100

    mise en forme

--- models/user.model.js
+++ models/user.model.js
@@ -1,47 +1,47 @@
-const mongoose = require('mongoose');
-const {isEmail} = require('validator');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema(
-    {
-        pseudo:{
-            type: String,
-            required:true,
-            minlength:3,
-            maxlength:55,
-            unique:true,
-            trimp: true
-        },
-        email: {
-            type:String,
-            required:true,
-            validate: [isEmail] ,
-            lowercase:true,
-            trim:true,
-        },
-        password :{
-            type: String,
-            required: true,
-            max:1024,
-            minlength:6,
-        },
-        bio:{
-            type: String,
-            max:1024,
-        },
-        followers: {
-            type: [String]
-        },
-        followings: {
-            type: [String]
-        },
-        Likes: {
-            type:[String]
-        },
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 55,
+      unique: true,
+      trimp: true,
     },
-    {
-        timestamps: true,
-    }
-)
-const UserModel = mongoose.model('user',userSchema);
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minlength: 6,
+    },
+    bio: {
+      type: String,
+      max: 1024,
+    },
+    followers: {
+      type: [String],
+    },
+    followings: {
+      type: [String],
+    },
+    Likes: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const UserModel = mongoose.model("user", userSchema);
 
 module.exports = UserModel;
