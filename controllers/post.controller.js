98d008111bf342b0883f4b7a02c819ae39c7ea56
henriB <henri.bretseau@gmail.com>
2020-11-27 10:39:23 +0100

    postRoutes CRUD

---
+++ controllers/post.controller.js
@@ -0,0 +1,16 @@
+const PostModel = require('../models/post.model');
+const UserModel = require('../models/user.model');
+const ObjectID = require('mongoose').Types.ObjectID;
+
+module.exports.readPost = (req, res) =>{
+
+}
+module.exports.createPost = (req, res) =>{
+
+}
+module.exports.updatePost = (req, res) =>{
+
+}
+module.exports.deletePost = (req, res) =>{
+    
+}
