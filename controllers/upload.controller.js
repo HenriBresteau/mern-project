df6be34d97f15881503e3d8b7e65e39bda217bb0
henriB <henri.bretseau@gmail.com>
2020-11-28 10:35:05 +0100

    Try fomat + poids de l'img correct

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,3 +1,24 @@
-module.exports.uploadProfil = ()=>{
+const UserModel = require("../models/user.model");
+const fs = require("fs");
+const { promisify } = require("util");
+const pipeline = promisify(require("stream").pipeline);
 
-}
+module.exports.uploadProfil = async (req, res) => {
+  try {
+    if (
+      //Format de l'image correct ?
+      req.file.detectedMimeType !== "image/jpg" &&
+      req.file.detectedMimeType !== "image/png" &&
+      req.file.detectedMimeType !== "image/jpeg"
+    ) {
+      throw Error("Invalid file");
+    }
+    if (req.file.size > 500000) {
+      throw Error("max size");
+    }
+  } catch (error) {
+    return res.satuts(201).json(error);
+  }
+
+  
+};
