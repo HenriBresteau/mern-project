3038dfa9c0637c506ae2edd1b71ffae164caeb2f
henriB <henri.bretseau@gmail.com>
2020-12-01 17:38:27 +0100

    Component Log

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Log from '../components/Log'
 const Profil = () => {
     return (
-        <div>
-            le profil
+        <div className='profil-page'>
+            <div className="log-container">
+                <Log ></Log>
+                <div className="img-container">
+                    <img src="./img/log.svg" alt="logo"/>
+                </div>
+            </div>
         </div>
     );
 };
