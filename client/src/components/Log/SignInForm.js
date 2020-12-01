8c444c0828551f287a0580ef3467b4b55de25783
henriB <henri.bretseau@gmail.com>
2020-12-01 20:32:36 +0100

    add dotenv

--- client/src/components/Log/SignInForm.js
+++ client/src/components/Log/SignInForm.js
@@ -11,7 +11,7 @@ const SignInForm = () => {
     const passwordError = document.querySelector(".password.error");
     axios({
       method: "post",
-      url: `${process.env.REACT_APP_API_URL}api/user/login`,
+      url: `${process.env.REACT_APP_API_URL}/api/user/login`,
       withCredentials: true,
       data: {
         email,
