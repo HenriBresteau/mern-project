947910b41a367e7a865f95e75fadf8b65612c632
henriB <henri.bretseau@gmail.com>
2020-12-01 17:50:19 +0100

    SignIn & SignUp

--- client/src/components/Log/index.js
+++ client/src/components/Log/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
+import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
 
-const index = () => {
-    return (
-        <div>
-            Log
-        </div>
-    );
+const Log = () => {
+  const [signUpModal, setSignUpModal] = useState(true);
+  const [signInModal, setSignInModal] = useState(false);
+  const handleModals = (e) => {
+    if (e.target.id === "register") {
+      setSignInModal(false);
+      setSignUpModal(true);
+    } else if (e.target.id === "login") {
+      setSignInModal(true);
+      setSignUpModal(false);
+    }
+  };
+  return (
+    <div className="connection-form">
+      <div className="form-container">
+        <ul>
+          <li
+            onClick={handleModals}
+            id="register"
+            className={signUpModal ? "active-btn" : null}
+          >
+            S'inscrire
+          </li>
+          <li
+            onClick={handleModals}
+            id="login"
+            className={signInModal ? "active-btn" : null}
+          >
+            Se connecter
+          </li>
+        </ul>
+        {signUpModal && <SignUpForm></SignUpForm>}
+        {signInModal && <SignInForm></SignInForm>}
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default Log;
