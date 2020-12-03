d57a95b3e81fe93a114b75f71c9e9dffdc79102d
henriB <henri.bretseau@gmail.com>
2020-12-03 18:33:22 +0100

    Dynamique avec Uid

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UidContext } from "./AppContext";
 
 const Navbar = () => {
+  const uid = useContext(UidContext);
   return (
     <nav>
       <div className="nav-container">
@@ -13,6 +15,26 @@ const Navbar = () => {
             </div>
           </NavLink>
         </div>
+        {uid ?(
+            <ul>
+                <li></li>
+                <li className="welcome">
+                    <NavLink exact to='/profil'>
+                        <h5>Bienvenue 'valeur dynamique</h5>
+                    </NavLink>
+                </li>
+                Logo logout
+            </ul>
+        ) : (
+            <ul>
+                <li></li>
+                <li>
+                    <NavLink exact to="/profil">
+                        <img src="./img/icons/login.svg" alt="login"/>
+                    </NavLink>
+                </li>
+            </ul>
+        )}
       </div>
       NAVABAR
     </nav>
