21479b37cef162e54f8b2b863e941389e409e379
henriB <henri.bretseau@gmail.com>
2020-12-03 20:15:38 +0100

    Reducer

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.scss';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from "redux-thunk";
+import rootReducer from './reducers'
+
+// devtools
+import {composeWithDevTools} from 'redux-devtools-extension';
+import logger from "redux-logger";
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk,logger))
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}> 
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
