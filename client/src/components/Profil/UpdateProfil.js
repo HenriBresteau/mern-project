19e2ba97a32221ea1060471a0a9fed800f7bb88a
henriB <henri.bretseau@gmail.com>
2020-12-04 11:04:26 +0100

    <UpdateProfil>

---
+++ client/src/components/Profil/UpdateProfil.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LeftNav from '../LeftNav';
+
+const UpdateProfil = () => {
+    return (
+        <div className="profil-container">
+            <LeftNav></LeftNav>
+        </div>
+    );
+};
+
+export default UpdateProfil;
